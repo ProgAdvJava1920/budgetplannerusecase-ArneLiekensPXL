22380aec68e895dc5292e5447f338d4256e42fc4
arnepxl <11703072@student.pxl.be>
2020-05-20 13:32:12 +0200

    REST implemented

--- src/main/java/be/pxl/student/rest/AccountsRest.java
+++ src/main/java/be/pxl/student/rest/AccountsRest.java
@@ -2,21 +2,26 @@ package be.pxl.student.rest;
 
 import be.pxl.student.dao.AccountDao;
 import be.pxl.student.dao.EntityManagerUtil;
+import be.pxl.student.dao.PaymentDao;
 import be.pxl.student.entity.Account;
+import be.pxl.student.entity.Payment;
 import be.pxl.student.rest.resource.AccountResource;
+import be.pxl.student.rest.resource.GetPaymentsResource;
+import be.pxl.student.rest.resource.PaymentResource;
 import jdk.jshell.spi.ExecutionControl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.cfg.NotYetImplementedException;
 
 import javax.persistence.EntityManager;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("accounts")
 public class AccountsRest {
@@ -57,8 +62,80 @@ public class AccountsRest {
     @POST
     @Path("{accountName}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createPayment() {
-        throw new NotYetImplementedException();
+    public Response createPayment(PaymentResource paymentResource, @PathParam("accountName") String accountName) {
+        EntityManager entityManager = EntityManagerUtil.createEntityManager();
+        AccountDao accountDao = new AccountDao(entityManager);
+        PaymentDao paymentDao = new PaymentDao(entityManager);
+
+        Account account = accountDao.getByName(accountName);
+        if(account == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Account does not exist").build();
+        }
+
+        Payment payment = mapPaymentResource(paymentResource);
+
+        if(payment == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("invalid argument").build();
+        }
+
+        Account counter = accountDao.getByIban(paymentResource.getCounterAccount());
+
+        if(counter == null) {
+            counter = new Account();
+            counter.setIBAN(paymentResource.getCounterAccount());
+            counter = accountDao.addAccount(counter);
+        }
+
+        payment.setCounterAccount(counter);
+        payment.setAccount(account);
+
+        paymentDao.addPayment(payment);
+
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{accountName}")
+    public Response getPayments(@PathParam("accountName") String accountName, @QueryParam("label") String labelName) {
+        Logger logger = LogManager.getLogger();
+        EntityManager entityManager = EntityManagerUtil.createEntityManager();
+        AccountDao accountDao = new AccountDao(entityManager);
+        PaymentDao paymentDao = new PaymentDao(entityManager);
+
+        Account account = accountDao.getByName(accountName);
+        if(account == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Account does not exist").build();
+        }
+
+        GetPaymentsResource paymentsResource = new GetPaymentsResource();
+
+        List<Payment> payments = null;
+        if(labelName != null) {
+            payments = new ArrayList<>();
+            for (Payment payment : account.getPayments()) {
+                if(payment.getLabel() != null) {
+                    if(payment.getLabel().getName().toLowerCase().equals(labelName.toLowerCase())) {
+                        payments.add(payment);
+                    }
+                }
+            }
+        } else {
+            payments = account.getPayments();
+        }
+        
+        paymentsResource.mapPayments(payments);
+        
+        float receiving = (float)payments.stream().filter(p -> p.getAmount() > 0).mapToDouble(Payment::getAmount).sum();
+        float spending = (float)payments.stream().filter(p -> p.getAmount() < 0).mapToDouble(Payment::getAmount).sum();
+
+        
+        paymentsResource.setReceivingAmount(receiving);
+        paymentsResource.setSpendingAmount(spending);
+        paymentsResource.setResultAmount(receiving + spending);
+
+        return Response.ok(paymentsResource).build();
+
     }
 
     private Account mapAccountResource(AccountResource accountResource) {
@@ -68,5 +145,31 @@ public class AccountsRest {
         account.setName(accountResource.getName());
 
         return account;
+    }
+
+    private Payment mapPaymentResource(PaymentResource paymentResource) {
+
+        try {
+            Payment payment = new Payment();
+
+            payment.setAmount(paymentResource.getAmount());
+            payment.setDetail(paymentResource.getDetail());
+
+            LocalDateTime localDateTime;
+            if(paymentResource.getDate() == null) {
+                localDateTime = LocalDateTime.now();
+            } else {
+                String[] datesplit = paymentResource.getDate().split("/");
+                localDateTime = LocalDateTime.of(Integer.parseInt(datesplit[2]), Integer.parseInt(datesplit[1]), Integer.parseInt(datesplit[0]), 0,0);
+            }
+
+            payment.setDate(localDateTime);
+
+            return payment;
+        } catch (Exception e) {
+            return null;
+        }
+
+
     }
 }
